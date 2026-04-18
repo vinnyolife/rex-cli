bba603616c0f20d62779e47d8f89f73705ff8b2c
Vinny Oliveira <vinnyolife@users.noreply.github.com>
2026-04-18 22:36:01 +0800

    add cli entry point

---
+++ bin/rex.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const { program } = require('commander');
+const pkg = require('../package.json');
+
+program
+  .name('rex')
+  .description(pkg.description || 'Rex CLI - a scaffolding and project management tool')
+  .version(pkg.version, '-v, --version', 'output the current version');
+
+// Create command
+program
+  .command('create <project-name>')
+  .description('Create a new project from a template')
+  .option('-t, --template <template>', 'specify a template to use', 'default')
+  .option('--no-install', 'skip dependency installation')
+  .action((projectName, options) => {
+    const { create } = require('../lib/commands/create');
+    create(projectName, options).catch(err => {
+      console.error('Error:', err.message);
+      process.exit(1);
+    });
+  });
+
+// Add command
+program
+  .command('add <component>')
+  .description('Add a component or plugin to the current project')
+  .option('-d, --dir <directory>', 'target directory', '.')
+  .action((component, options) => {
+    const { add } = require('../lib/commands/add');
+    add(component, options).catch(err => {
+      console.error('Error:', err.message);
+      process.exit(1);
+    });
+  });
+
+// List templates command
+program
+  .command('list')
+  .alias('ls')
+  .description('List available templates')
+  .action(() => {
+    const { list } = require('../lib/commands/list');
+    list().catch(err => {
+      console.error('Error:', err.message);
+      process.exit(1);
+    });
+  });
+
+// Info command
+program
+  .command('info')
+  .description('Print environment and project info for debugging')
+  .action(() => {
+    const { info } = require('../lib/commands/info');
+    info();
+  });
+
+// Handle unknown commands
+program.on('command:*', ([cmd]) => {
+  console.error(`Unknown command: ${cmd}`);
+  console.log('Run rex --help for a list of available commands.');
+  process.exit(1);
+});
+
+program.parse(process.argv);
+
+// Show help if no args provided
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
